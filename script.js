ae90889da502e8c30614c72fde4cd93b84254382
Cintia Osorio Duarte <cintia.o.duarte@gmail.com>
2024-12-16 20:57:08 -0300

    Agrego archivo Javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+function mostrarMensaje() {
+    alert('¡Hola, bienvenido a Alfajores Arami!');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    mostrarMensaje();
+});
