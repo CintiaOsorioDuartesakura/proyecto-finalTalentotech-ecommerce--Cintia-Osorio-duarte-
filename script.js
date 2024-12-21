db4d68b6834742f53fe53765613cbf47d0c13115
Cintia Osorio Duarte <cintia.o.duarte@gmail.com>
2024-12-20 22:36:28 -0300

    creando productos js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
 // script.js
 
 let Productos = [
-   
+
     { "id":maicena,
         "name": 'maicena',
         "cantidad":('media docena','1 docena','2 docenas'), 
@@ -23,11 +23,32 @@ let Productos = [
         "img": "maicena.jpg",
         "description" : "Los alfajores de maicena son un clásico de la pastelería argentina. Estos alfajores delicados y suaves están hechos con una masa de maicena que se deshace en la boca, rellenos de dulce de leche y cubiertos con una generosa capa de coco rallado. Cada bocado es un deleite de sabores y texturas que evoca los recuerdos más dulces de la infancia."
         },
-       
+    {
+        "id": cordobeces,
+        "name": 'cordobeces',
+        "cantidad":('media docena','1 docena','2 docenas'),
+        "price" : (8500,17000,34000),
+        "img": "cordobeces.jpg",
+        "description" : "Los alfajores cordobeses destacan por su particular textura y sabor. Con una receta que incluye harina de trigo y miel, estos alfajores están rellenos de una generosa capa de dulce de leche y cubiertos con un glaseado blanco que les da un toque distintivo. Son conocidos por su equilibrio perfecto entre dulzura y suavidad, ofreciendo una experiencia de sabor auténticamente argentina."
+    },
+    
 
-    { cordobeces: 'cordobeces', precio: 200 },
-    { nombre: 'Producto 3', precio: 300 },
-    { nombre: 'Producto 4', precio: 400 },
+    {
+        "id": marplatenses,
+        "name": 'marplatenses',
+        "cantidad":('media docena','1 docena','2 docenas'),
+        "price" : (9000,18000,36000),
+        "img": "mdq.jpg",
+        "description" : "El alfajor marplatense es famoso por su inigualable combinación de sabores. Estos alfajores están formados por dos galletas esponjosas rellenas de dulce de leche, bañadas en un suave chocolate semi amargo que se derrite en la boca. Originarios de Mar del Plata, estos alfajores son un verdadero símbolo de la tradición repostera argentina, perfectos para cualquier ocasión."
+    },
+    {
+        "id": colaciones,
+        "name": 'colaciones',
+        "cantidad":('media docena','1 docena','2 docenas'),
+        "price" : (10000,20000,40000),
+        "img": "colaciones.jpg",
+        "description" : "Los alfajores argentinos son una maravilla de la pastelería argentina. Estos alfajores delicados y suaves están hechos con una masa de maicena que se deshace en la boca, rellenos de dulce de leche y cubiertos con una generosa capa de coco rallado. Cada bocado es un deleite de sabores y texturas que evoca los recuerdos más dulces de la infancia."
+    }
     { nombre: 'Producto 5', precio: 500 }
 ];
 
